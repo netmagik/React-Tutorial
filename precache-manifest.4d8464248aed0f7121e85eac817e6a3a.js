dbc111186fb9f57cf759473f9c68b316fd072b3d
Irina <irina@netmagik.com>
2020-08-13 12:25:46 -0400

    Updates

---
+++ precache-manifest.4d8464248aed0f7121e85eac817e6a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745b96efd0997b8760fb1d020c0ae9c2",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "7c998d14a9078536846a",
+    "url": "/React-Tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "88f424e5fe702ead4086",
+    "url": "/React-Tutorial/static/js/2.ff6b7496.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/React-Tutorial/static/js/2.ff6b7496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c998d14a9078536846a",
+    "url": "/React-Tutorial/static/js/main.55057dc9.chunk.js"
+  },
+  {
+    "revision": "f4a3984e544610f2b6d1",
+    "url": "/React-Tutorial/static/js/runtime-main.32939327.js"
+  }
+]);
