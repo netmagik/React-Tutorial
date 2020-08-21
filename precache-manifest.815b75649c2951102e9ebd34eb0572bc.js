57980396026fb9d95fd8fadc8e7f96b70c42408d
Irina <irina@netmagik.com>
2020-08-20 20:09:30 -0400

    Updates

---
+++ precache-manifest.815b75649c2951102e9ebd34eb0572bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6395334c458467fc913b34fdecda7f",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "0844a779ba2a43d0a056",
+    "url": "/React-Tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/React-Tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0844a779ba2a43d0a056",
+    "url": "/React-Tutorial/static/js/main.98917821.chunk.js"
+  },
+  {
+    "revision": "f4a3984e544610f2b6d1",
+    "url": "/React-Tutorial/static/js/runtime-main.32939327.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Tutorial/precache-manifest.4d8464248aed0f7121e85eac817e6a3a.js"
+  "/React-Tutorial/precache-manifest.815b75649c2951102e9ebd34eb0572bc.js"
 );
 
 self.addEventListener('message', (event) => {
